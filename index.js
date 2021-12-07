d551c85b5e198c68497ba0c0142ee88f99206182
Shir <shir@marketing66.com>
2021-12-07 10:58:29 +0200

    'init'

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const server = http.createServer(app);
 const { Server, Socket } = require("socket.io");
 const io = new Server(server);
 
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
-
+  
   io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
